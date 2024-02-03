08f927784ac5cb12de9eecbff7311f3cb801eac3
Erfan Heshmati <heshmati74@gmail.com>
2024-02-03 13:33:43 +0330

    add modals to profile page

--- src/components/infoCard/InfoCard.jsx
+++ src/components/infoCard/InfoCard.jsx
@@ -1,13 +1,49 @@
 import { UilPen } from "@iconscout/react-unicons"
 import "./InfoCard.css"
+import { useState } from "react"
+import { Modal } from '@mantine/core';
 
 export default function InfoCard() {
+    const [modalOpened, setModalOpened] = useState(false)
+
     return (
         <div className="InfoCard">
             <div className="infoHead">
                 <h4>Your Info</h4>
                 <div>
-                    <UilPen width="2rem" height="1.2rem" />
+                    <UilPen width="2rem" height="1.2rem" onClick={() => setModalOpened(true)} />
+                    <Modal
+                        overlayOpacity={0.55}
+                        overlayBlur={3}
+                        size="55%"
+                        opened={modalOpened}
+                        onClose={() => setModalOpened(false)}
+                    >
+                        <form className="infoForm">
+                            <h3>Your info</h3>
+                            <div>
+                                <input type="text" name="firstname" placeholder="First Name" className="infoInput" />
+                                <input type="text" name="lasttname" placeholder="Last Name" className="infoInput" />
+                            </div>
+                            <div>
+                                <input type="text" name="worksAt" placeholder="Works At" className="infoInput" />
+                            </div>
+                            <div>
+                                <input type="text" name="livesIn" placeholder="Lives In" className="infoInput" />
+                                <input type="text" name="country" placeholder="Country" className="infoInput" />
+                            </div>
+                            <div>
+                                <input type="text" name="relationship" placeholder="Relationship Status" className="infoInput" />
+                            </div>
+                            <div>
+                                Profile Image
+                                <input type="file" name="profileImage" />
+                                Cover Image
+                                <input type="file" name="coverImage" />
+                            </div>
+                            <button className="btn info-btn">Update</button>
+                        </form>
+                    </Modal>
                 </div>
             </div>
 
--- src/components/rightSide/RightSide.jsx
+++ src/components/rightSide/RightSide.jsx
@@ -4,8 +4,13 @@ import Noti from "../../img/noti.png"
 import Comment from "../../img/comment.png"
 import { UilSetting } from "@iconscout/react-unicons"
 import TrendCard from "../trendCard/TrendCard"
+import { useState } from "react"
+import { Modal } from '@mantine/core';
+import PostShare from "../postShare/PostShare"
 
 export default function RightSide() {
+    const [modalOpened, setModalOpened] = useState(false)
+
     return (
         <div className="RightSide">
             <div className="navIcons">
@@ -15,7 +20,16 @@ export default function RightSide() {
                 <img src={Comment} alt="" />
             </div>
             <TrendCard />
-            <button className="btn r-btn">Share</button>
+            <button className="btn r-btn" onClick={() => setModalOpened(true)}>Share</button>
+            <Modal
+                overlayOpacity={0.55}
+                overlayBlur={3}
+                size="55%"
+                opened={modalOpened}
+                onClose={() => setModalOpened(false)}
+            >
+                <PostShare />
+            </Modal>
         </div>
     )
 }
