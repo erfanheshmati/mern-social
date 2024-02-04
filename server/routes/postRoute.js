d27ca436fabca3d7a118e1051833a8b60e6c7e57
Erfan Heshmati <heshmati74@gmail.com>
2024-02-04 17:53:53 +0330

    complete post controller

--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,7 +1,16 @@
 import express from "express";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  likePost,
+  updatePost,
+} from "../controllers/postController.js";
 
 const router = express.Router();
-router.get("/", async (req, res) => {
-  res.send("post route");
-});
+router.post("/create", createPost);
+router.get("/get/:id", getPost);
+router.put("/update/:id", updatePost);
+router.delete("/delete/:id", deletePost);
+router.put("/like/:id", likePost);
 export default router;
