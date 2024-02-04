be7d6b1c7e447b8c397ae469bee9ac3c0a73988a
Erfan Heshmati <heshmati74@gmail.com>
2024-02-04 13:40:35 +0330

    complete user controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,105 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const getUser = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await userModel.findById(id);
+    if (user) {
+      const { password, ...otherDetails } = user._doc;
+      res.status(200).json(otherDetails);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus, password } = req.body;
+  if (id === currentUserId || currentUserAdminStatus) {
+    try {
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+      const user = await userModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res
+      .status(403)
+      .json({ message: "Access Denied! You can only update your own profile" });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus } = req.body;
+  if (id === currentUserId || currentUserAdminStatus) {
+    try {
+      const user = await userModel.findByIdAndDelete(id);
+      res.status(200).json("User deleted successfully");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res
+      .status(403)
+      .json({ message: "Access denied! You can only delete your own profile" });
+  }
+};
+
+export const followUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId } = req.body;
+  if (id === currentUserId) {
+    res
+      .status(403)
+      .json({ message: "Action forbidden! You can not follow yourself" });
+  } else {
+    try {
+      const followUser = await userModel.findById(id);
+      const followingUser = await userModel.findById(currentUserId);
+      if (!followUser.followers.includes(currentUserId)) {
+        await followUser.updateOne({ $push: { followers: currentUserId } });
+        await followingUser.updateOne({ $push: { followings: id } });
+        res.status(200).json("User followed");
+      } else {
+        res.status(403).json("User is already followed by you");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+};
+
+export const unfollowUser = async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId } = req.body;
+    if (id === currentUserId) {
+      res
+        .status(403)
+        .json({ message: "Action forbidden! You can not follow yourself" });
+    } else {
+      try {
+        const followUser = await userModel.findById(id);
+        const followingUser = await userModel.findById(currentUserId);
+        if (followUser.followers.includes(currentUserId)) {
+          await followUser.updateOne({ $pull: { followers: currentUserId } });
+          await followingUser.updateOne({ $pull: { followings: id } });
+          res.status(200).json("User unfollowed");
+        } else {
+          res.status(403).json("User is not followed by you");
+        }
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    }
+  };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,16 @@
 import express from "express";
-import { getUser } from "../controllers/userController.js";
+import {
+  deleteUser,
+  followUser,
+  getUser,
+  unfollowUser,
+  updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
-router.get("/:id", getUser);
+router.get("/get/:id", getUser);
+router.put("/update/:id", updateUser);
+router.delete("/delete/:id", deleteUser);
+router.put("/follow/:id", followUser);
+router.put("/unfollow/:id", unfollowUser);
 export default router;
