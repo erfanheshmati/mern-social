73aa7549ed3e05c1d50f1e85ba39c92e0e5f111c
Erfan Heshmati <heshmati74@gmail.com>
2024-02-06 10:46:42 +0330

    setup jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   const salt = await bcrypt.genSalt(10);
@@ -12,8 +13,16 @@ export const registerUser = async (req, res) => {
     if (oldUser) {
       return res.status(400).json("This username is already registered!");
     }
-    const result = await newUser.save();
-    res.status(200).json(result);
+    const user = await newUser.save();
+    const token = jwt.sign(
+      {
+        username: user.username,
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -25,9 +34,19 @@ export const loginUser = async (req, res) => {
     const user = await userModel.findOne({ username: username });
     if (user) {
       const validity = await bcrypt.compare(password, user.password);
-      validity
-        ? res.status(200).json(user)
-        : res.status(400).json({ message: "Wrong Password" });
+      if (!validity) {
+        res.status(400).json("Wrong password!");
+      } else {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            id: user._id,
+          },
+          process.env.JWT_KEY,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
+      }
     } else {
       res.status(404).json({ message: "User does not exist" });
     }
