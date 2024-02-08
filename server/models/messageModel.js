a026169ef9e7ced781ac3e06149da6f0c39331ba
Erfan Heshmati <heshmati74@gmail.com>
2024-02-08 13:41:48 +0330

    create chat and message model

---
+++ server/models/messageModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    chatId: {
+      type: String,
+    },
+    senderId: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const messageModel = mongoose.model("Message", messageSchema);
+export default messageModel;
